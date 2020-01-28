ef9fec4f072faaa98cfc6f340488318074ab5c17
Sergey Voytov <sergvoytov@gmail.com>
2020-01-28 10:40:57 -0800

    1

---
+++ src/main/java/com/sergeyvoytov/CodeFellowship/CodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.sergeyvoytov.CodeFellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergeyvoytov/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeyvoytov.CodeFellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
